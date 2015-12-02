5cb73041c0ca1e212724c994a140a4d36e389e1b
Joel Azoulay <joelkazoulay@gmail.com>
2015-12-01 20:48:08 -0700

    EP - changed selector to everything-pigs

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
-	selector: 'my-app',
+	selector: 'everything-pigs',
 	template: '<h1>My First Angular 2 App</h1>'
 })
 class AppComponent { }

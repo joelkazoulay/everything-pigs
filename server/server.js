1a04ad833cf8cf585e6d374a9e7f062150f09924
Joel Azoulay <joelkazoulay@gmail.com>
2015-12-04 10:05:02 -0700

    EP - implemented typecript compiler and static file server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var compression = require('compression');
+
 var app = express();
 
 var oneDay = 86400000;
 
-var logger = function(req, res, next) {
-  console.log("GOT REQUEST !");
-  next(); // Passing the request to the next handler in the stack.
-};
-
-app.use(logger); // Here you add your logger to the stack.
-
 // compress all requests
 app.use(compression());
 
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/../src', { maxAge: oneDay }));
 
-app.listen(5678);
-console.log('Server running at ' + 5678);
+var server = app.listen(5678, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
